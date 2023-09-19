fcd04d4f40d5a9ea5ce264b31e7752d780f751c1
Quentin FLAGEOLLET <quentin.flageollet1@etu.univ-lorraine.fr>
2023-09-19 15:46:07 +0200

    Exercice 1 ServiceCommande

--- shop.pizza-shop/src/domain/service/classes/ServiceCatalogue.php
+++ shop.pizza-shop/src/domain/service/classes/ServiceCatalogue.php
@@ -6,7 +6,6 @@ use domain\service\interfaces\ICatalogue;
 
 class ServiceCatalogue implements ICatalogue
 {
-  // fait un constructeur qui passe en paramètre le service de commande
     private ServiceCommande $serviceCommande;
 
     public function __construct(ServiceCommande $serviceCommande)
--- shop.pizza-shop/src/domain/service/classes/ServiceCommande.php
+++ shop.pizza-shop/src/domain/service/classes/ServiceCommande.php
@@ -1,14 +1,34 @@
 <?php
 namespace domain\service\classes;
 use domain\service\interfaces\ICommande;
+use domain\exception\ServiceCommandeNotFoundException;
 
 class ServiceCommande implements ICommande
 {
-    // fait le constructeur qui passe en parammètre le service de catalogue
     private ServiceCatalogue $serviceCatalogue;
 
     public function __construct(ServiceCatalogue $serviceCatalogue)
     {
         $this->serviceCatalogue = $serviceCatalogue;
     }
+
+    public function readCommande(String $UUID): CommandeDTO
+    {
+        if($UUID) {
+            return new CommandeDTO();
+        }else{
+            throw new ServiceCommandeNotFoundException();
+        }
+    }
+
+    public function validateCommande(String $UUID): CommandeDTO
+    {
+        if($UUID) {
+            return new CommandeDTO();
+        }else{
+            throw new ServiceCommandeNotFoundException();
+        }
+    }
+
+
 }
