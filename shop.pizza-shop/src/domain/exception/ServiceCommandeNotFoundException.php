e9ff249cfc5c86c3ac58bc212fdcc41982206481
Quentin FLAGEOLLET <quentin.flageollet1@etu.univ-lorraine.fr>
2023-09-19 15:47:53 +0200

    class ServiceCommandeNotFoundException

---
+++ shop.pizza-shop/src/domain/exception/ServiceCommandeNotFoundException.php
@@ -0,0 +1,9 @@
+<?php
+
+
+class ServiceCommandeNotFoundException extends Exception{
+    public function __construct(string $message = "Erreur : la commande n'existe pas", int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
