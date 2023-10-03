4dd9870157dc8495324f90ff6d2a1ddbe2db2d53
Elian <arnaud35u@etu.univ-lorraine.fr>
2023-10-03 15:09:40 +0200

    Add files to config

--- shop.pizza-shop/config/routes.php
+++ shop.pizza-shop/config/routes.php
@@ -2,10 +2,18 @@
 declare(strict_types=1);
 
 use pizzashop\shop\app\actions\AccessOrderAction;
+use pizzashop\shop\app\actions\AccessOrderApiAction;
 use pizzashop\shop\app\actions\CreateOrderAction;
 use pizzashop\shop\app\actions\CreateOrderApiAction;
+use pizzashop\shop\app\actions\ValidateOrderApiAction;
+use Slim\App;
 
-return function( \Slim\App $app):void {
+return function( App $app):void {
+
+    $app->get('/', function ($request, $response, $args) {
+        $response->getBody()->write("Hello world!");
+        return $response;
+    });
 
     $app->post('/orders[/]', CreateOrderAction::class)
         ->setName('create_orders');
@@ -15,4 +23,10 @@ return function( \Slim\App $app):void {
 
     $app->get('/api/create-order[/]', CreateOrderApiAction::class)
         ->setName('create_order_api');
+
+    $app->get('/api/orders/{id_order}[/]', AccessOrderApiAction::class)
+        ->setName('access_order_api');
+
+    $app->patch('/api/orders/{id_order}[/]', ValidateOrderApiAction::class)
+        ->setName('validate_order_api');
 };
