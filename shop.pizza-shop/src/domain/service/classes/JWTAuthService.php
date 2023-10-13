1a9ee5dd8faec7f4c746d3fd1cd068ccf3cfed94
toinou887 <rionde8u@etu.univ-lorraine.fr>
2023-10-13 19:14:24 +0200

    Ajout de la classe de service d'authentification JWT

---
+++ shop.pizza-shop/src/domain/service/classes/JWTAuthService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace pizzashop\shop\domain\service\classes;
+
+class JWTAuthService
+{
+    private AuthService $authProvider;
+    private JWTManager $jwtManager;
+
+    public function __construct($authProvider, $jwtManager) {
+        $this->authProvider = $authProvider;
+        $this->jwtManager = $jwtManager;
+    }
+
+    public function signIn($username, $password) {
+        $user = $this->authProvider->verifyCredentials($username, $password);
+        if ($user) {
+            $tokenData = ['username' => $user->username, 'email' => $user->email];
+            $accessToken = $this->jwtManager->createToken($tokenData);
+            $refreshToken = $this->jwtManager->createToken(['refresh_token' => $user->refresh_token]);
+            return ['access_token' => $accessToken, 'refresh_token' => $refreshToken];
+        }
+        return null;
+    }
+
+    public function validate($accessToken) {
+        $decodedToken = $this->jwtManager->validateToken($accessToken);
+        if ($decodedToken && isset($decodedToken['upr'])) {
+            return $decodedToken['upr'];
+        }
+        return null;
+    }
+
+    public function refresh($refreshToken) {
+        $user = $this->authProvider->verifyRefreshToken($refreshToken);
+        if ($user) {
+            return $this->signIn($user->username, $user->password);
+        }
+        return null;
+    }
+}
