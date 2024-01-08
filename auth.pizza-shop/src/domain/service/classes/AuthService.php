35e4669de6ee767b695345e0fa10d25390021a32
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2024-01-08 23:19:32 +0100

    Authentification provider rebase

--- auth.pizza-shop/src/domain/service/classes/AuthService.php
+++ auth.pizza-shop/src/domain/service/classes/AuthService.php
@@ -3,77 +3,100 @@
 namespace pizzashop\auth\api\domain\service\classes;
 
 use Carbon\Carbon;
+use PHPUnit\Framework\Exception;
+use pizzashop\auth\api\domain\dto\auth\UserDTO;
+use pizzashop\auth\api\domain\entities\auth\User;
+use pizzashop\auth\api\domain\exceptions\CredentialsException;
+use pizzashop\auth\api\domain\exceptions\TokenException;
+use pizzashop\auth\api\domain\exceptions\UserException;
 use pizzashop\auth\api\domain\service\interfaces\IAuth;
 
 class AuthService implements IAuth
 {
-    private $db;
-
-    public function __construct($db) {
-        $this->db = $db;
-    }
-
-    public function verifyCredentials($username, $password)
+    /**
+     * @throws CredentialsException
+     */
+    public function verifyCredentials($email, $password)
     {
-        $user = $this->db->table('users')->where('username', $username)->first();
+        $user = User::where('email', $email)->first();
         if ($user && password_verify($password, $user->password)) {
             return $user;
         }
-        return null;
+        throw new CredentialsException();
     }
 
+    /**
+     * @throws TokenException
+     */
     public function verifyRefreshToken($refreshToken)
     {
-        $user = $this->db->table('users')->where('refresh_token', $refreshToken)->first();
-        return $user ? $user : null;
+        return User::where('refresh_token', $refreshToken)->first() ?: throw new TokenException('Invalid refresh token');
     }
 
     public function getUserByEmail($email)
     {
-        $user = $this->db->table('users')->where('email', $email)->first();
-        return $user ? $user : null;
+        return User::where('email', $email)->first() ?: null;
     }
 
     public function createUser($username, $email, $password)
     {
-        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
-        $activationToken = bin2hex(random_bytes(16));
+        try {
+            $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+            $activationToken = bin2hex(random_bytes(16));
 
-        $user = [
-            'username' => $username,
-            'email' => $email,
-            'password' => $hashedPassword,
-            'activation_token' => $activationToken,
-            'activation_token_expiration_date' => Carbon::now()->addMinutes(5)->toDateTimeString(),
-        ];
+            $user = [
+                'username' => $username,
+                'email' => $email,
+                'password' => $hashedPassword,
+                'activation_token' => $activationToken,
+                'activation_token_expiration_date' => Carbon::now()->addMinutes(5)->toDateTimeString(),
+            ];
 
-        $userId = $this->db->table('users')->insertGetId($user);
-        return $userId ? $this->db->table('users')->find($userId) : null;
+            return User::create($user);
+        } catch (Exception $e) {
+            throw new UserException("Error during user creation");
+        }
 
     }
 
+    /**
+     * @throws UserException
+     */
     public function verifyActivationToken($activationToken)
     {
-        $user = $this->db->table('users')
-            ->where('activation_token', $activationToken)
+        $user = User::where('activation_token', $activationToken)
             ->where('activation_token_expiration_date', '>', Carbon::now())
             ->first();
-        return $user ? $user : null;
+        return $user ?: throw new UserException('Invalid activation token');
     }
 
-    public function activateUserAccount($id)
+    /**
+     * @throws UserException
+     */
+    public function activateUserAccount($email)
     {
-        return $this->db->table('users')->where('id', $id)->update([
-            'active' => 1,
-            'activation_token' => null,
-            'activation_token_expiration_date' => null,
-        ]);
-
+        try {
+            $user = User::find($email);
+            $user->active = 1;
+            $user->activation_token = null;
+            $user->activation_token_expiration_date = null;
+            return $user->save();
+        } catch (Exception) {
+            throw new UserException("Error during user activation");
+        }
     }
 
-    public function getAuthenticatedUserProfile($userId)
+    /**
+     * @throws UserException
+     */
+    public function getAuthenticatedUserProfile($username, $email, $refreshToken) : UserDTO
     {
-        // TODO: Implement getAuthenticatedUserProfile() method.
+        try {
+            $user = $this->getUserByEmail($email);
+            return $user->toDTO();
+        } catch (Exception $e) {
+            throw new UserException("Error during user profile retrieval");
+        }
     }
 
     public function register($username, $email, $password)
