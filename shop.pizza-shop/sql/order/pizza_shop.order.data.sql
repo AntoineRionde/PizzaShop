9148c3541fc0347fd8e7fd2ba206ff8e6e5b3eb6
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2024-01-16 15:49:28 +0100

    DB automatisation

--- shop.pizza-shop/sql/order/pizza_shop.order.data.sql
+++ shop.pizza-shop/sql/order/pizza_shop.order.data.sql
@@ -5,6 +5,43 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+DROP TABLE IF EXISTS `commande`;
+CREATE TABLE `commande` (
+                            `delai` tinyint(4) DEFAULT 0,
+                            `id` varchar(64) NOT NULL,
+                            `date_commande` datetime NOT NULL,
+                            `type_livraison` int(11) NOT NULL DEFAULT 1,
+                            `etat` int(11) NOT NULL DEFAULT 1,
+                            `montant_total` decimal(10,2) NOT NULL DEFAULT 0.00,
+                            `mail_client` varchar(128) NOT NULL,
+                            KEY `id_client` (`mail_client`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE `item` (
+                        `id` int(11) NOT NULL AUTO_INCREMENT,
+                        `numero` int(11) NOT NULL,
+                        `libelle` varchar(32) NOT NULL,
+                        `taille` int(11) NOT NULL,
+                        `libelle_taille` varchar(32) NOT NULL,
+                        `tarif` decimal(6,2) NOT NULL,
+                        `quantite` int(11) NOT NULL,
+                        `commande_id` varchar(64) NOT NULL,
+                        PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- 2023-09-06 14:47:17
+
+
+-- Adminer 4.8.1 MySQL 5.5.5-10.3.11-MariaDB-1:10.3.11+maria~bionic dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
 INSERT INTO `commande` (`delai`, `id`, `date_commande`, `type_livraison`, `etat`, `montant_total`, `mail_client`) VALUES
 (0,	'112e7ee1-3e8d-37d6-89cf-be3318ad6368',	'2023-05-17 01:46:37',	3,	1,	61.91,	'ThéodoreLeger@sfr.fr'),
 (0,	'9b5e8d97-2be9-3c2a-a33f-86305746f1e0',	'2022-10-17 17:39:52',	3,	1,	39.92,	'RenéePeron@yahoo.fr'),
