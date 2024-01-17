bad97595e883b92be8040302b8bb7aa508d365d0
toinou887 <rionde8u@etu.univ-lorraine.fr>
2024-01-17 12:00:36 +0100

    Ajout des 2 autres script rabbitMQ

--- shop.pizza-shop/src/console/consomate_message.php
+++ shop.pizza-shop/src/console/consomate_message.php
@@ -3,31 +3,32 @@
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
-use PhpAmqpLib\Message\AMQPMessage;
 
-// Connect to RabbitMQ server
-$connection = new AMQPStreamConnection('rabbitmq', 5672, 'admin', '@admin1#!');
+$connection = null;
+try {
+    $connection = new AMQPStreamConnection('rabbitmq', 5672, 'admin', '@admin1#!');
+} catch (Exception $e) {
+}
+
+$message_queue = 'nouvelles_commandes';
+
 $channel = $connection->channel();
 
-// Declare the queue if it doesn't exist
-$channel->queue_declare('nouvelles_commandes', false, false, false, false);
+$msg = $channel->basic_get($message_queue);
 
-// Consume a message from the queue in blocking mode
-echo " [*] Waiting for messages. To exit press CTRL+C\n";
-$callback = function (AMQPMessage $msg) {
+if ($msg) {
     echo " [x] Received message: ";
     $messageContent = json_decode($msg->body, true);
     print_r($messageContent);
     echo "\n";
 
-    // Acknowledge the message reception
-    $msg->getChannel()->basic_ack($msg->getDeliveryTag());
-};
-$channel->basic_consume('nouvelles_commandes', '', false, true, false, false, $callback);
-
-while ($channel->is_open()) {
-    $channel->wait();
+    $channel->basic_ack($msg->getDeliveryTag());
+} else {
+    echo "No message in queue\n";
 }
 
 $channel->close();
-$connection->close();
+try {
+    $connection->close();
+} catch (Exception $e) {
+}
---
+++ shop.pizza-shop/src/console/setupRMQ.php
@@ -0,0 +1,21 @@
+<?php
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$exchange_name = 'pizzashop';
+$queue_name = 'nouvelles_commandes';
+$routing_key = 'nouvelle';
+$connection = null;
+try {
+    $connection = new AMQPStreamConnection('rabbitmq', 5672, 'admin', '@admin1#!');
+} catch (Exception $e) {
+}
+$channel = $connection->channel();
+$channel->exchange_declare($exchange_name, 'direct', false, true, false);
+$channel->queue_declare($queue_name, false, true, false, false);
+$channel->queue_bind($queue_name, $exchange_name, $routing_key);
+$channel->close();
+try {
+    $connection->close();
+} catch (Exception $e) {
+}
