176bcae6079fa031ad1cb0b50438a7032b4609cb
toinou887 <rionde8u@etu.univ-lorraine.fr>
2024-02-02 15:06:09 +0100

    Modif de ValidateOrderAction.php

--- shop.pizza-shop/src/app/actions/ValidateOrderAction.php
+++ shop.pizza-shop/src/app/actions/ValidateOrderAction.php
@@ -3,6 +3,7 @@
 namespace pizzashop\shop\app\actions;
 
 use Exception;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 use pizzashop\shop\domain\exception\OrderNotFoundException;
 use pizzashop\shop\domain\exception\OrderRequestInvalidException;
@@ -18,20 +19,19 @@ class ValidateOrderAction extends AbstractAction
         $this->os = $container->get('order.service');
     }
 
-    public function __invoke($request, $response, $args)
+    public function __invoke($request, $response, $args): \Slim\Psr7\Response|\Slim\Psr7\Message
     {
         $response = $this->addCorsHeaders($response);
 
         try {
             $this->os->validateOrder($args['id_order']);
-            // Connexion à RabbitMQ
+
             $connection = new AMQPStreamConnection('rabbitmq', 5672, 'admin', '@admin1#!');
             $channel = $connection->channel();
 
-            // Déclaration de la queue
             $channel->queue_declare('nouvelles_commandes', false, false, false, false);
-            // Valeur a modifier pour $jsonOrder (recuperer la commande et l'encoder en json)
-            $jsonOrder = json_encode(['id_order' => $args['id_order']]);
+            $orderJson = $this->os->readOrder($args['id_order']);
+            $jsonOrder = json_encode($orderJson->toArray());
             $message = new AMQPMessage($jsonOrder);
             $channel->basic_publish($message, '', 'nouvelles_commandes');
 
