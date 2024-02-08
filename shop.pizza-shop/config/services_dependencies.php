393ed40aefef8d7df66a19c65ba26d81792afa4e
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2024-02-08 19:55:43 +0100

    Rework - Services Dependencies

--- shop.pizza-shop/config/services_dependencies.php
+++ shop.pizza-shop/config/services_dependencies.php
@@ -1,20 +1,20 @@
 <?php
 
-use pizzashop\shop\domain\service\classes\CatalogService;
-use Psr\Container\ContainerInterface;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
+use pizzashop\shop\domain\service\classes\CatalogService;
+use Psr\Container\ContainerInterface;
 
 return [
     'log.order.name' => 'order.log',
-    'log.order.file' => __DIR__ . '/../logs/order.log',
+    'log.order.file' => '/var/www/logs/order.log',
     'log.order.level' => Monolog\Logger::DEBUG,
 
     'catalog.service' => function (ContainerInterface $c) {
         return new CatalogService();
     },
 
-    'order.logger' => function(ContainerInterface $container) {
+    'order.logger' => function (ContainerInterface $container) {
         $logger = new Logger($container->get('log.order.name'));
         $logger->pushHandler(new StreamHandler($container->get('log.order.file'), $container->get('log.order.level')));
         return $logger;
