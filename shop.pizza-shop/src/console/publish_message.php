f378f07faa94d0ab14ea638c67eeee217bcb85a9
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2024-02-11 01:07:31 +0100

    Rework - Generation Order

--- shop.pizza-shop/src/console/publish_message.php
+++ shop.pizza-shop/src/console/publish_message.php
@@ -13,15 +13,32 @@ use Ramsey\Uuid\Nonstandard\Uuid;
 function generateRandomOrder(): array
 {
     $faker = Factory::create();
-    return [
-        'id' => Uuid::uuid4(),
-        'mail_client' => $faker->email,
-        'date_commande' => $faker->dateTimeThisMonth()->format('Y-m-d H:i:s'),
-        'type_livraison' => rand(1, 3),
-        'delai' => rand(0, 10),
-        'etat' => Order::CREATED,
-        'montant_total' => $faker->randomFloat(2, 10, 100)
+
+    $order = [
+        'id' => $faker->uuid,
+        'date' => $faker->dateTimeThisMonth()->format('Y-m-d H:i:s'),
+        'livraisonType' => rand(1, 3),
+        'totalAmount' => round(rand(10, 100), 2),
+        'clientMail' => $faker->email,
+        'delay' => rand(0, 10),
+        'items' => [],
     ];
+
+    $numProducts = rand(1, 5);
+    for ($i = 0; $i < $numProducts; $i++) {
+        $order['items'][] = [
+            'id' => $faker->randomNumber(3),
+            'numero' => $i + 1,
+            'libelle' => $faker->word,
+            'taille' => rand(1, 3),
+            'libelle_taille' => $faker->randomElement(['petite', 'normale', 'grande']),
+            'tarif' => round(rand(5, 20), 2),
+            'quantite' => rand(1, 5),
+            'commande_id' => $order['id'],
+        ];
+    }
+
+    return $order;
 }
 
 
